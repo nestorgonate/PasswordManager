da96f0c69ad92351182c9d92833965d28731ede7
Nestor Gavilanes <negavilaneson@uide.edu.ec>
2025-03-07 20:16:57 -0500

    Eliminar addacounts.py del repositorio

--- addaccounts.py
+++
@@ -1,7 +0,0 @@
-import json
-from accountsfile import verify_accountfile
-def addAccounts():
-    if verify_accountfile():
-        try:
-        except Exception as jsone:
-            print(jsone)
